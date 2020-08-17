a6bf2a55caf73b7265790915cec3b2329f58f14b
rl-king <hello@rubenlieking.com>
2020-08-17 21:53:11 +0200

    Move codegen, add init cl args

---
+++ src/backend/Positive/CodeGen.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Positive.CodeGen where
+
+import Data.Foldable (for_)
+import Data.HashMap.Strict as HashMap
+import Data.List as List
+import Data.Text as Text
+import qualified Language.Elm.Expression as Expression
+import qualified Language.Elm.Pretty as Pretty
+import qualified Language.Elm.Simplification as Simplification
+import qualified Language.Haskell.To.Elm as Elm
+import Positive.Server
+import Positive.Settings
+import Servant.API.Generic (ToServantApi)
+import qualified Servant.To.Elm
+import qualified System.Directory as Directory
+import qualified System.Exit
+import System.FilePath.Posix ((</>))
+import qualified System.FilePath.Posix as Path
+import System.Log.FastLogger (TimedFastLogger)
+import qualified System.Process as Process
+
+-- CODEGEN
+
+codeGen :: TimedFastLogger -> IO ()
+codeGen logger = do
+  let endpointDefinitions =
+        fmap (Servant.To.Elm.elmEndpointDefinition (Expression.String "") ["Generated", "Request"]) $
+          Servant.To.Elm.elmEndpoints @(ToServantApi SettingsApi)
+      jsonDefinitions =
+        Elm.jsonDefinitions @ImageSettings
+          <> Elm.jsonDefinitions @ImageCrop
+      modules =
+        Pretty.modules $
+          Simplification.simplifyDefinition
+            <$> jsonDefinitions <> endpointDefinitions
+  logMsg_ logger "Removing src/frontend/Generated before generating code"
+  Directory.removeDirectoryRecursive "src/frontend/Generated"
+  for_ (HashMap.toList modules) $ \(modulePath, contents) -> do
+    (filename, location) <-
+      case List.reverse modulePath of
+        [] -> pure ("", "")
+        [moduleName] -> do
+          let p = "src/frontend"
+          Directory.createDirectoryIfMissing True p
+          pure (Text.unpack $ moduleName <> ".elm", p)
+        (moduleName : rest) -> do
+          let p = Path.joinPath . fmap Text.unpack $ "src" : "frontend" : List.reverse rest
+          Directory.createDirectoryIfMissing True p
+          pure (Text.unpack $ moduleName <> ".elm", p)
+    writeFile (location </> filename) (show contents)
+    logMsg_ logger $ "Wrote elm file: " <> Text.pack (location </> filename)
+  runElmFormat logger
+
+runElmFormat :: TimedFastLogger -> IO ()
+runElmFormat logger = do
+  let args = ["--elm-version=0.19", "--yes", "src/frontend/Generated"]
+  result <- Process.withCreateProcess (Process.proc "elm-format" args) $
+    \_ _ _ handler -> Process.waitForProcess handler
+  case result of
+    System.Exit.ExitSuccess -> logMsg_ logger "Formatted generated code with elm-format"
+    _ -> logMsg_ logger $ "Something went wrong trying to format the generated Elm code: " <> tshow result
--- src/backend/app/Main.hs
+++ src/backend/app/Main.hs
@@ -1,71 +1,52 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import Data.Foldable (for_)
-import Data.HashMap.Strict as HashMap
-import Data.List as List
+import Control.Monad (when)
 import Data.Text as Text
-import qualified Language.Elm.Expression as Expression
-import qualified Language.Elm.Pretty as Pretty
-import qualified Language.Elm.Simplification as Simplification
-import qualified Language.Haskell.To.Elm as Elm
+import Options.Applicative
+import Positive.CodeGen
 import Positive.Server
-import Positive.Settings
-import Servant.API.Generic (ToServantApi)
-import qualified Servant.To.Elm
-import qualified System.Directory as Directory
-import qualified System.Exit
-import System.FilePath.Posix ((</>))
-import qualified System.FilePath.Posix as Path
-import System.Log.FastLogger (TimedFastLogger)
 import qualified System.Log.FastLogger as FastLogger
-import qualified System.Process as Process
+
+-- MAIN
 
 main :: IO ()
 main = do
   timeCache <- FastLogger.newTimeCache FastLogger.simpleTimeFormat
-  FastLogger.withTimedFastLogger
-    timeCache
-    (FastLogger.LogStdout FastLogger.defaultBufSize)
-    (\logger -> codeGen logger >> server logger)
+  FastLogger.withTimedFastLogger timeCache (FastLogger.LogStdout FastLogger.defaultBufSize) $
+    \logger -> do
+      Flags {fDir, fCodeGen} <- parseArgs
+      when fCodeGen (codeGen logger)
+      server logger
 
-codeGen :: TimedFastLogger -> IO ()
-codeGen logger = do
-  let endpointDefinitions =
-        fmap (Servant.To.Elm.elmEndpointDefinition (Expression.String "") ["Generated", "Request"]) $
-          Servant.To.Elm.elmEndpoints @(ToServantApi SettingsApi)
-      jsonDefinitions =
-        Elm.jsonDefinitions @ImageSettings
-          <> Elm.jsonDefinitions @ImageCrop
-      modules =
-        Pretty.modules $
-          Simplification.simplifyDefinition
-            <$> jsonDefinitions <> endpointDefinitions
-  logMsg_ logger "Removing src/frontend/Generated before generating code"
-  Directory.removeDirectoryRecursive "src/frontend/Generated"
-  for_ (HashMap.toList modules) $ \(modulePath, contents) -> do
-    (filename, location) <-
-      case List.reverse modulePath of
-        [] -> pure ("", "")
-        [moduleName] -> do
-          let p = "src/frontend"
-          Directory.createDirectoryIfMissing True p
-          pure (Text.unpack $ moduleName <> ".elm", p)
-        (moduleName : rest) -> do
-          let p = Path.joinPath . fmap Text.unpack $ "src" : "frontend" : List.reverse rest
-          Directory.createDirectoryIfMissing True p
-          pure (Text.unpack $ moduleName <> ".elm", p)
-    writeFile (location </> filename) (show contents)
-    logMsg_ logger $ "Wrote elm file: " <> Text.pack (location </> filename)
-  runElmFormat logger
+-- FLAGS
 
-runElmFormat :: TimedFastLogger -> IO ()
-runElmFormat logger = do
-  let args = ["--elm-version=0.19", "--yes", "src/frontend/Generated"]
-  result <- Process.withCreateProcess (Process.proc "elm-format" args) $
-    \_ _ _ handler -> Process.waitForProcess handler
-  case result of
-    System.Exit.ExitSuccess -> logMsg_ logger "Formatted generated code with elm-format"
-    _ -> logMsg_ logger $ "Something went wrong trying to format the generated Elm code: " <> tshow result
+data Flags = Flags
+  { fDir :: !Text,
+    fCodeGen :: !Bool
+  }
+  deriving (Show, Eq)
+
+parseArgs :: IO Flags
+parseArgs =
+  customExecParser (prefs showHelpOnError) $
+    info (parser <**> helper) (fullDesc <> progDesc "positive")
+
+parser :: Parser Flags
+parser =
+  Flags
+    <$> parseDir
+    <*> flag False True (long "codegen")
+
+parseDir :: Parser Text
+parseDir =
+  option auto $
+    long "dir"
+      <> short 'd'
+      <> showDefault
+      <> value "./"
+      <> metavar "DIR"
+      <> help "Directory to use"
