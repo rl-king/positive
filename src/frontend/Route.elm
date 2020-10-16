df87b0b2e3fca131efe18cdb2db6497211814806
rl-king <hello@rubenlieking.com>
2020-10-16 14:39:26 +0200

    Handle unnested page path

--- src/frontend/Route.elm
+++ src/frontend/Route.elm
@@ -43,6 +43,9 @@ fromUrl url =
                     Url.Parser.s "editor"
                         </> Url.Parser.string
                         </> Url.Parser.string
+                , Url.Parser.map (toEditorRoute ".") <|
+                    Url.Parser.s "editor"
+                        </> Url.Parser.string
                 , Url.Parser.map (\x -> Browser { minimumRating = x }) <|
                     Url.Parser.top
                         <?> Url.Parser.Query.int "rating"
