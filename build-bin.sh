5d6d2b907c5d1399a028d28f5994b7fd19950a15
rl-king <hello@rubenlieking.com>
2021-01-01 14:11:16 +0100

    Add bin stack compilation args

--- build-bin.sh
+++ build-bin.sh
@@ -3,4 +3,4 @@
 elm make src/frontend/Main.elm  --output=dist/main.js --optimize;
 cp src/frontend/style.css dist;
 cp -r src/frontend/icons dist;
-stack build --ghc-options='-O2' --copy-bins
+stack build --ghc-options='-O2 -j +RTS -A128m -n2m -RTS' --copy-bins
