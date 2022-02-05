59da9d908ee8307d45943d5089585339abc8e956
rl-king <hello@rubenlieking.com>
2022-02-05 17:50:30 +0100

    Add hls info

--- build-bin.sh
+++ build-bin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 hpack &&
-cabal exec codegen &&
+cabal exec pos-codegen &&
 elm make src/frontend/Main.elm  --output=dist/main.js --optimize &&
 cp index.html dist &&
 cp src/frontend/style.css dist &&
 cp -r src/frontend/icons dist &&
-cabal install positive:exe:pos -O2 -j --overwrite-policy=always
+cabal install positive:exe:pos -O2 -j --overwrite-policy=always --ghc-options="-j +RTS -A64m -n2m -RTS"
--- build-dev.sh
+++ build-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 hpack &&
-cabal exec codegen &&
+cabal exec pos-codegen &&
 elm make src/frontend/Main.elm  --output=dist/main.js --debug &&
 cp index.html dist &&
 cp src/frontend/style.css dist &&
 cp -r src/frontend/icons dist &&
-cabal build positive:exe:pos -O0 -j
+cabal build positive:exe:pos -O0 --ghc-options="-j +RTS -A64m -n2m -RTS"
